8c825b2d04b4cff5a75733f41111be6d41094ced
Talm28 <100356396+Talm28@users.noreply.github.com>
2024-10-11 16:46:03 +0300

    Add ant stop mechanism.

--- Assets/Scripts/AntMovement.cs
+++ Assets/Scripts/AntMovement.cs
@@ -7,12 +7,15 @@ public class AntMovement : MonoBehaviour
     [SerializeField] private Vector2 _speed;
     [SerializeField] float _noiseScaleMax;
     [SerializeField] private Vector2 _size;
+    [SerializeField] private Vector2 _stopTime;
 
     private float _currSpeed;
     private Vector3 _target;
     private float _noiseYValue;
     private float _timer;
+    private float _stopAndTimer;
     private bool _canMove;
+    private bool _isStopped;
     private float _noiseScale;
     private float _maxDisntance;
 
@@ -30,9 +33,12 @@ public class AntMovement : MonoBehaviour
         _noiseYValue = Random.Range(0f, 1000f);
 
         _timer = 0;
+        _stopAndTimer = 0;
 
         float size = Random.Range(_size.x, _size.y);
         transform.localScale = new Vector3(size, size, size);
+
+        _isStopped = false;
     }
 
     // Update is called once per frame
@@ -43,7 +49,10 @@ public class AntMovement : MonoBehaviour
         if(_canMove)
         {
             Rotate(_target);
-            Movement();
+            if(! _isStopped)
+                Movement();
+                
+            StopGenerator();
         }
     }
 
@@ -84,5 +93,35 @@ public class AntMovement : MonoBehaviour
 
         // Rotate the ant
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+    }
+
+    private void StopGenerator()
+    {
+        if(_isStopped) return;
+
+        _stopAndTimer += Time.deltaTime;
+
+        if(_stopAndTimer > 1)
+        {
+            _stopAndTimer = 0;
+            if(Random.Range(0,5) == 0)
+                StopAnt();
+        }
+
+        
+    }
+
+    private void StopAnt()
+    {
+        _isStopped = true;
+
+        float waitTime = Random.Range(_stopTime.x, _stopTime.y);
+
+        StartCoroutine(StopAndCoroutine(waitTime));
+    }
+    private IEnumerator StopAndCoroutine(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        _isStopped = false;
     }
 }
